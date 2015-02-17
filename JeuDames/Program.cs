ab6fc6994903ea9039750ebbf233c8e23b10d730
myc1986 <moratinos.c@gmail.com>
2015-02-17 17:00:17 +0100

    Init

--- JeuDames/Program.cs
+++ JeuDames/Program.cs
@@ -9,7 +9,7 @@ namespace JeuDames
     static class Program
     {
         /// <summary>
-        /// Point d'entrée principal de l'application.
+        /// Point d'entrée principal de l'application. b
         /// </summary>
         [STAThread]
         static void Main()
